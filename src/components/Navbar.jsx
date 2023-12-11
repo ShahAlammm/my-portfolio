763235900a6f764aa766e3ffeb480975c762160a
Shah Alam <shahalam01660166344@gmail.com>
2023-12-11 12:36:14 +0600

    added components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,93 @@
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { useState } from "react";
+import logo from "../../public/lotto.json";
+import Lottie from "lottie-react";
+import { menu, close } from "../assets";
+
+const items = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const Navbar = () => {
+  const [active, setActive] = useState(" ");
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to={"/"}
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <Lottie animationData={logo} className="w-16 h-16 object-contain" />
+          <p className="text-white pt-3 text-[18px] font-bold cursor-pointer">
+            Shah Alam
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10 text-white">
+          {items.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-lg`}
+          >
+            <ul className="list-none flex flex-col justify-end items-center gap-4 text-white">
+              {items.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } font-serif text-[16px] font-medium cursor-pointer`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,10 @@
+
+const Computers = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Computers;
