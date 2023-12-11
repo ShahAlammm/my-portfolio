ba35727566cf4a08b1d2f020f5a8ace923a5f657
Shah Alam <shahalam01660166344@gmail.com>
2023-12-11 23:43:33 +0600

    added email js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,8 +15,45 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
-  const handleSubmit = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_walfvvk",
+        "template_48cx9x4",
+        {
+          from_name: form.name,
+          to_name: "Shah Alam",
+          from_email: form.email,
+          to_email: "shahalam01660166344@gmail.com",
+          message: form.message,
+        },
+        "7PLMishgZkYzVusaP"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible");
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert("Something went wrong");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
@@ -80,7 +117,7 @@ const Contact = () => {
         variants={slideIn("right", "tween", 0.2, 1)}
         className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px] "
       >
-        <EarthCanvas/>
+        <EarthCanvas />
       </motion.div>
     </div>
   );
